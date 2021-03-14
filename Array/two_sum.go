5272004dcf713ebfcb2c7568518bcb573d6a8a31
allen <suleiauyu@163.com>
2021-03-14 22:28:36 +0800

    Add：两数之和，新增一种解法

--- Array/two_sum.go
+++ Array/two_sum.go
@@ -6,6 +6,7 @@ import (
 )
 
 // TwoSum 在数组中找到 2 个数之和等于给定值的数字，结果返回 2 个数字在数组中的下标
+// 对每一个元素，计算其另一半数值，循环查询另一半是否在数组中，查到则返回下标
 func TwoSum(nums []int, target int) []int {
 	fmt.Println(nums)
 	fmt.Println(target)
@@ -14,7 +15,7 @@ func TwoSum(nums []int, target int) []int {
 		num1 := nums[i]
 		num2 := target - num1
 		index, found := common.FindInt(nums, num2)
-		if found && i != index{
+		if found && i != index {
 			results = append(results, i)
 			results = append(results, index)
 			return results
@@ -23,5 +24,17 @@ func TwoSum(nums []int, target int) []int {
 	return results
 }
 
-// result := Array.TwoSum([]int{2, 7, 11, 15}, 9)
-// fmt.Println(result)
+// TwoSum2 在数组中找到 2 个数之和等于给定值的数字，结果返回 2 个数字在数组中的下标
+// 对每一个元素，计算其另一半数值，判断另一半是否在map中，如果在返回下标，不在则把该元素存入map中
+func TwoSum2(nums []int, target int) []int {
+	temp_map := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		num1 := nums[i]
+		num2 := target - num1
+		if _, ok := temp_map[num2]; ok {
+			return []int{temp_map[num2], i}
+		}
+		temp_map[num1] = i
+	}
+	return nil
+}
